8c0519aa70b213f658958d383870c8e9c40a7680
Ozan KANIK <l1110018@std.yildiz.edu.tr>
2019-09-01 10:22:12 +0200

    Production Factory Created

---
+++ JMovies.IMDb/Factories/ProductionFactory.cs
@@ -0,0 +1,29 @@
+﻿using JM.Entities.Interfaces;
+using JMovies.IMDb.Entities.Movies;
+
+namespace JMovies.IMDb.Factories
+{
+    /// <summary>
+    /// Factory class that is responsible for building productions based on the type
+    /// </summary>
+    public class ProductionFactory : IFactory<Production, ProductionTypeEnum>
+    {
+        /// <summary>
+        ///  Build method of Production Factory that produces the given type of productions
+        /// </summary>
+        /// <param name="type">Type of production to be produced</param>
+        /// <returns>New production instance</returns>
+        public Production Build(ProductionTypeEnum type)
+        {
+            switch (type)
+            {
+                case ProductionTypeEnum.Movie:
+                    return new Movie();
+                case ProductionTypeEnum.TVSeries:
+                    return new TVSeries();
+                default:
+                    return new Production();
+            }
+        }
+    }
+}
